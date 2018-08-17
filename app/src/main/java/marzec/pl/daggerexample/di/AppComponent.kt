e25789bee2598e8355bedc0035d59cfe3e9d194c
Mariusz Marzec <mariusz.marzec@miquido.com>
2018-08-17 16:18:05 +0200

    Producers

--- app/src/main/java/marzec/pl/daggerexample/di/AppComponent.kt
+++ app/src/main/java/marzec/pl/daggerexample/di/AppComponent.kt
@@ -1,20 +1,41 @@
 package marzec.pl.daggerexample.di
 
+import com.google.common.util.concurrent.ListenableFuture
 import dagger.Component
 import dagger.MembersInjector
 import dagger.Module
 import dagger.Provides
+import dagger.producers.ProducerModule
+import dagger.producers.Produces
+import dagger.producers.ProductionComponent
 import marzec.pl.daggerexample.App
+import dagger.producers.Production
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
-@Component(modules = [AppModule::class])
+
+@ProductionComponent(modules = [AppModule::class, ExecutorModule::class])
 interface AppComponent {
 
     fun inject(app: App)
+
+    fun getUserName(): ListenableFuture<UserName>
 }
 
 @Module
+object ExecutorModule {
+    @Provides
+    @Production
+    fun executor(): Executor {
+        return Executors.newCachedThreadPool()
+    }
+}
+@ProducerModule(includes = [ExecutorModule::class])
 class AppModule {
 
-    @Provides
-    fun provideUserName() : UserName = UserName()
+    @Produces
+    fun provideUserName() : UserName {
+        Thread.sleep(10 * 1000)
+        return UserName()
+    }
 }
