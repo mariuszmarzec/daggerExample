4250618c0a1c8aedf8fb69b993e4bb21ac31fa7c
Mariusz Marzec <mariusz.marzec@miquido.com>
2018-08-20 23:08:00 +0200

    Injections - Optional

--- app/src/main/java/marzec/pl/daggerexample/ui/main/MainActivity.kt
+++ app/src/main/java/marzec/pl/daggerexample/ui/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package marzec.pl.daggerexample.ui.main
 
+import android.annotation.TargetApi
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import marzec.pl.daggerexample.App
 import marzec.pl.daggerexample.R
 import marzec.pl.daggerexample.di.UserName
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Provider
 
@@ -16,6 +19,10 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var userName: Provider<UserName>
 
+    @Inject
+    lateinit var nickName: Optional<NickName?>
+
+    @TargetApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,7 +30,13 @@ class MainActivity : AppCompatActivity() {
         (application as App).appComponent.inject(this)
 
         helloWorldButton.setOnClickListener {
-            Toast.makeText(this, userName.get().text, Toast.LENGTH_SHORT).show()
+            val nick = nickName.orElse(null)
+            val name = if (nick != null) nick.text else userName.get().text
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
         }
     }
+}
+
+class NickName {
+    val text = "nick"
 }
