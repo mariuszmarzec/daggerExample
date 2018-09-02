888bc79b44380f6d9878b202f054cff432de98d0
Mariusz Marzec <mariusz.marzec@miquido.com>
2018-09-02 15:06:24 +0200

    Multibindings

--- app/src/main/java/marzec/pl/daggerexample/di/AppComponent.kt
+++ app/src/main/java/marzec/pl/daggerexample/di/AppComponent.kt
@@ -2,30 +2,94 @@ package marzec.pl.daggerexample.di
 
 import dagger.*
 import java.util.*
+import dagger.Provides
+import dagger.multibindings.*
+import marzec.pl.daggerexample.ui.main.MainActivity
+import kotlin.collections.Set
+
 
 @Component(modules = [AppModule::class])
 interface AppComponent {
 
-//    fun getInt(): Int
     fun mainActivityComponent(): MainActivityComponent
 }
 
 @Module
-class AppModule {
+class AppModule
 
-    @Provides
-    @Reusable
-    fun provideInt(): Int = randomInt()
+@Subcomponent(modules = [MainActivityModule::class,
+SetModule::class, StringModule::class, ClassKeyModule::class, CustomMapKeyModule::class])
+interface MainActivityComponent {
+    fun inject(mainActivity: MainActivity)
 }
 
-@Subcomponent(modules = [MainActivityModule::class])
-interface MainActivityComponent {
+@Module
+class SetModule {
+    @Provides
+    @IntoSet
+    fun provideOneString(): String {
+        return "ABC"
+    }
 
-    fun getInt(): Int
+    @Provides
+    @ElementsIntoSet
+    fun provideSomeStrings(): Set<String> {
+        return setOf("DEF", "GHI")
+    }
+}
+
+@Module
+class StringModule {
+    @Provides
+    @IntoMap
+    @StringKey("1")
+    fun provideStringToMap(): String {
+        return "Jeden"
+    }
+
+    @Provides
+    @IntoMap
+    @StringKey("2")
+    fun provideStringToMap2(): String {
+        return "Dwa"
+    }
+
+}
+
+@Module
+class ClassKeyModule {
+    @Provides
+    @IntoMap
+    @ClassKey(Long::class)
+    fun provideLongMaxValue() : String {
+        return Long.MAX_VALUE.toString()
+    }
+
+    @Provides
+    @IntoMap
+    @ClassKey(Short::class)
+    fun provideShortMaxValue() : String {
+        return Short.MAX_VALUE.toString()
+    }
+}
+
+@Module
+class CustomMapKeyModule {
+    @Provides
+    @IntoMap
+    @ExampleEnumKey(ExampleEnum.NUMBER_1)
+    fun provideStringWithEnum(): Int {
+        return 1
+    }
 }
 
 @Module
 class MainActivityModule {
+
 }
 
-fun randomInt() = Random().nextInt(1000)
+enum class ExampleEnum {
+    NUMBER_1, NUMBER_2
+}
+@MapKey
+annotation class ExampleEnumKey(val value: ExampleEnum)
--- app/src/main/java/marzec/pl/daggerexample/ui/main/MainActivity.kt
+++ app/src/main/java/marzec/pl/daggerexample/ui/main/MainActivity.kt
@@ -3,31 +3,51 @@ package marzec.pl.daggerexample.ui.main
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import dagger.MapKey
 import kotlinx.android.synthetic.main.activity_main.*
 import marzec.pl.daggerexample.App
 import marzec.pl.daggerexample.R
+import marzec.pl.daggerexample.di.ExampleEnum
+import java.lang.StringBuilder
+import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var setOfStrings: Set<String>
+
+    @Inject
+    lateinit var mapStringToString: Map<String, String>
+
+    @Inject
+    lateinit var mapMaxValues: Map<Class<*>, String>
+
+    @Inject
+    lateinit var mapEnumToIn: Map<ExampleEnum, Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val appComponent = (application as App).appComponent
-        val mainActivityComponent = appComponent.mainActivityComponent()
-        val mainActivityComponent2 = appComponent.mainActivityComponent()
+        appComponent.mainActivityComponent().inject(this)
 
         singletonButton.setOnClickListener {
-            //TODO uncomment getInt() for cache object in app component
-//            Toast.makeText(this, appComponent.getInt().toString(), Toast.LENGTH_SHORT).show()
+            val text = setOfStrings.fold(StringBuilder()) { sb, it -> sb.append(it)}.toString()
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
         }
 
         activityScopeButton.setOnClickListener {
-            Toast.makeText(this, mainActivityComponent.getInt().toString(), Toast.LENGTH_SHORT).show()
+            val text = mapStringToString.entries.fold(StringBuilder()) { sb, it ->
+                sb.append(it.key).append(" ").append(it.value).append("\n")
+            }.toString()
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
         }
 
         secondComponentButton.setOnClickListener {
-            Toast.makeText(this, mainActivityComponent2.getInt().toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, mapMaxValues[Long::class.java], Toast.LENGTH_SHORT).show()
         }
     }
+
 }
